33f4e8a1df2b7030e227ac1f9056020a438d5ec9
pimier15 <pimier15@gmail.com>
2017-10-31 13:55:59 +0900

    Image File Update

--- DataAnalysis/DataAnalysis/IPS/RandomForeset/IPSRandomForest.py
+++ DataAnalysis/DataAnalysis/IPS/RandomForeset/IPSRandomForest.py
@@ -53,6 +53,7 @@ Position = np.concatenate((p2,p3))
 ########## Fitting ###########
 
 forest.fit(XTrain,yTrain) # Use Train Data for Train
+
 predict = forest.predict(X) # Prediction of All Datas. use for calc error and correlation
 y = y.flatten()
 errors = mean_squared_error(y , predict)
@@ -85,6 +86,7 @@ stream.close()
 
 
 
+
 ################ Display ###################
 print("Error : ",errors)
 print("Correlation : " , core)
@@ -93,9 +95,9 @@ plt.title( "Correlation = {0} , Error = {1},  (Red : #2 , Blue : #4) ".format(co
 plt.xlabel( "Predict" )
 plt.ylabel( "Target" )
 s1 = plt.scatter(pre2Train , y2Train , c = 'r' , alpha = 0.5 , label = "#2Train")
-s2 = plt.scatter(pre3Train , y3Train , c = 'b' , alpha = 0.5 , label = "#3Train")
+s2 = plt.scatter(pre3Train , y3Train , c = 'b' , alpha = 0.5 , label = "#4Train")
 s3 = plt.scatter(pre2Test , y2Test   , c = 'g' , alpha = 0.9 , label = "#2Test")
-s4 = plt.scatter(pre3Test , y3Test   , c = 'y' , alpha = 0.9 , label = "#3Test")
+s4 = plt.scatter(pre3Test , y3Test   , c = 'y' , alpha = 0.9 , label = "#4Test")
 s5 = plt.scatter( [285]*len(yTrain) , yTrain   , c = 'brown' , alpha = 0.9 , label = "Target")
 plt.plot( predict , fit[0]*predict + fit[1] , color = 'Turquoise')
 plt.legend()
